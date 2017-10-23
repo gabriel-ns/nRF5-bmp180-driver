647e095d2911503941ac85d60cd780e5c452dfa1
Gabriel Nascimento <gns.1512@yahoo.com.br>
2017-10-23 04:17:32 -0200

    defining the new oop interface for htu21

--- Libs/htu21d/nrf5-htu21d-drv.h
+++ Libs/htu21d/nrf5-htu21d-drv.h
@@ -1,6 +1,14 @@
 #ifndef NRF5_HTU21D_LIB_H
 #define NRF5_HTU21D_LIB_H
 
+typedef enum htu21d_state
+{
+    HTU21D_STATE_IDLE,
+    HTU21D_STATE_TEMP_CONV,
+    HTU21D_STATE_HUM_CONV,
+    HTU21D_STATE_ERROR
+}htu21d_state_t;
+
 typedef enum htu21d_resolution
 {
     HTU21D_RES_RH_12_TEMP_14 = 0x02,
@@ -9,6 +17,47 @@ typedef enum htu21d_resolution
     HTU21D_RES_RH_11_TEMP_11 = 0x83,
 }htu21d_resolution_t;
 
+typedef enum htu21d_evt_type
+{
+    HTU21D_NO_EVT,
+    HTU21D_EVT_DATA_READY,
+    HTU21D_EVT_ERROR,
+    HTU21D_EVT_CRIT_ERROR
+}htu21d_evt_type_t;
+
+
+typedef struct htu21d htu21d_t;
+
+typedef struct htu21d_evt_data
+{
+        htu21d_evt_type_t evt_type;
+        htu21d_t * htu21d;
+        ret_code_t err_code;
+}htu21d_evt_data_t;
+
+typedef void (*htu21d_event_cb_t)(htu21d_evt_data_t * event_data);
+
+struct htu21d
+{
+        nrf_drv_twi_t * p_twi;
+        htu21d_resolution_t resolution;
+        htu21d_state_t state;
+        uint16_t humidity;
+        int16_t temperature;
+        htu21d_evt_data_t last_evt;
+};
+
+
+ret_code_t htu21d_drv_begin(htu21d_event_cb_t (* htu21d_event_cb)(htu21d_evt_data_t * event_data));
+ret_code_t htu21d_drv_start_sensor(htu21d_t * htu, nrf_drv_twi_t * p_twi, htu21d_resolution_t resolution);
+ret_code_t htu21d_drv_convert_data(htu21d_t * htu);
+ret_code_t htu21d_drv_set_resolution(htu21d_t * htu, htu21d_resolution_t resolution);
+ret_code_t htu21d_drv_reset_sensor(htu21d_t * htu);
+ret_code_t htu21d_drv_get_sensor_id(htu21d_t * htu, uint8_t * id_buff);
+
+
+
+#if 0
 ret_code_t htu21d_drv_begin(nrf_drv_twi_t * p_ext_twi, htu21d_resolution_t res);
 ret_code_t htu21d_drv_convert_temp_hold();
 ret_code_t htu21d_drv_convert_temp_no_hold();
@@ -22,5 +71,6 @@ uint16_t htu21d_drv_get_hum_conversion_time();
 uint16_t htu21d_calculate_rh(uint16_t buffer);
 int16_t htu21d_calculate_temperature(uint16_t buffer);
 ret_code_t htu21d_get_last_conversion(uint16_t * buffer);
+#endif
 
 #endif // NRF5_HTU21D_LIB_H
