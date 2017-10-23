d46c18a99a1e0fb5559447f12fff5f0f124deedd
Gabriel Nascimento <gns.1512@yahoo.com.br>
2017-10-23 03:18:20 -0200

    removed the previous interface

--- Libs/bmp180/nrf5-bmp180-drv.h
+++ Libs/bmp180/nrf5-bmp180-drv.h
@@ -70,27 +70,4 @@ ret_code_t bmp180_drv_convert_data(bmp180_t * bmp);
 ret_code_t bmp180_drv_set_pwr_mode(bmp180_t * bmp, bmp180_pwr_mode_t pwr_mode);
 ret_code_t bmp180_drv_reset_sensor(bmp180_t * bmp);
 ret_code_t bmp180_drv_get_sensor_id(bmp180_t * bmp, uint8_t * id_buff);
-
-#if(0)
-ret_code_t bmp180_drv_begin(nrf_drv_twi_t * p_ext_twi, bmp180_pwr_mode_t pwr_mode);
-
-ret_code_t bmp180_drv_convert_temp();
-
-ret_code_t bmp180_drv_convert_pres();
-
-ret_code_t bmp180_drv_get_pres(int32_t * app_buffer);
-
-ret_code_t bmp180_drv_get_temp(int16_t * app_buffer);
-
-ret_code_t bmp180_drv_set_pwr_mode(bmp180_pwr_mode_t pwr_mode);
-
-bmp180_pwr_mode_t bmp180_drv_get_pwr_mode();
-
-ret_code_t bmp180_drv_reset();
-
-ret_code_t bmp180_drv_get_id(uint8_t * id_buff);
-
-uint16_t bmp180_drv_get_conv_time();
-
-#endif
 #endif /** NRF5_BMP180_LIB_H */
