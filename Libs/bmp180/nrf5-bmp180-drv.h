b5e012c93d294365e6c2303a1f94c861b027e1f1
Gabriel Nascimento <gns.1512@yahoo.com.br>
2017-10-23 01:19:47 -0200

    - Defined a new sensor object structure

--- Libs/bmp180/nrf5-bmp180-drv.h
+++ Libs/bmp180/nrf5-bmp180-drv.h
@@ -10,7 +10,18 @@ typedef enum pwr_mode
     BMP180_SHUTDOWN
 }bmp180_pwr_mode_t;
 
-ret_code_t  bmp180_drv_begin(nrf_drv_twi_t * p_ext_twi, bmp180_pwr_mode_t pwr_mode);
+typedef struct bmp180
+{
+        nrf_drv_twi_t * p_twi;
+        bmp180_pwr_mode_t pwr_mode;
+        static app_timer_t bmp180_internal_timer_data;
+        static const app_timer_id_t bmp180_internal_timer;
+        uint16_t pressure;
+        int16_t temperature;
+}bmp180_t;
+
+#if(0)
+ret_code_t bmp180_drv_begin(nrf_drv_twi_t * p_ext_twi, bmp180_pwr_mode_t pwr_mode);
 
 ret_code_t bmp180_drv_convert_temp();
 
@@ -30,4 +41,5 @@ ret_code_t bmp180_drv_get_id(uint8_t * id_buff);
 
 uint16_t bmp180_drv_get_conv_time();
 
+#endif
 #endif /** NRF5_BMP180_LIB_H */
